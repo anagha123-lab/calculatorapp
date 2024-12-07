301f2c13e344efb1e45378a0d0adc2f420f8613b
Anagha Kulkarni <anaghk12@gmail.com>
2024-12-07 23:48:56 +0530

    Final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import '../src/comps/Calculator.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import Calculator from './comps/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+       <Calculator/>
+    </div>
+  );
+}
+
+export default App;
